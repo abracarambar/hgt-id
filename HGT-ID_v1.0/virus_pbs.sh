9a9d5d4915350bf815b72f7da75ad07dbf503767
Maely Gauthier <mg3342@raijin3.(none)>
2018-11-15 22:31:28 +1100

    add pbs script

--- HGT-ID_v1.0/virus_pbs.sh
+++ HGT-ID_v1.0/virus_pbs.sh
@@ -21,9 +21,6 @@
 ### Make the data readable to everyone in the group
 #PBS -W umask=027
 
-### Set out (Use job ID so it's unique)
-#PBS -e /g/data1a/jp48/scripts/transposon/ocscc/MakeVCF_LINE1.${ERRFILE}_${TASK_ID}
-
 ### Request nodes, memory, walltime. NB THESE ARE REQUIRED.
 ##### run, change these entries
 #PBS -l ncpus=4
@@ -56,7 +53,7 @@ cd ${PBS_O_WORKDIR}
 
 #/g/data1a/jp48/scripts/hgtid/HGT-ID_v1.0/bin/samtools/samtools index /g/data1a/jp48/gdc-client.1.3/bams/https\:/api.gdc.cancer.gov/data/53f0ce53-95fe-4d6c-930d-f8427c0c46ec/TCGA-CV-5442-01A-01D-2266-10_Illumina.bam
 
-perl /g/data1a/jp48/scripts/hgtid/HGT-ID_v1.0/src/hgt_adapted2.pl -b /g/data1a/jp48/gdc-client.1.3/bams/https:/api.gdc.cancer.gov/data/53f0ce53-95fe-4d6c-930d-f8427c0c46ec/TCGA-CV-5442-01A-01D-2266-10_Illumina.bam -s TCGA-CV-5442 -c  /g/data1a/jp48/scripts/hgtid/HGT-ID_v1.0/config.txt -o /g/data1a/jp48/scripts/hgtid/TCGA-CV-5442
+perl /g/data1a/jp48/scripts/hgtid/HGT-ID_v1.0/src/hgt_adapted2.pl -b /g/data1a/jp48/young_cohort/TCGA-CV-5442-01A-01D-2266-10_Illumina.bam -s TCGA-CV-5442 -c  /g/data1a/jp48/scripts/hgtid/HGT-ID_v1.0/config.txt -o /g/data1a/jp48/scripts/hgtid/TCGA-CV-5442
 #perl /g/data1a/jp48/scripts/hgtid/HGT-ID_v1.0/src/hgt_adapted.pl -b /g/data1a/jp48/young_cohort/TCGA-BA-5557/bb531fb4-064f-47dd-8231-bd6131866968/TCGA-BA-5557-01A-01D-1509_120420_SN1120_0135_BD0T5FACXX_s_4_rg.sorted.bam -s TCGA-BA-5557 -c /g/data1a/jp48/scripts/hgtid/HGT-ID_v1.0/config.txt -o /g/data1a/jp48/scripts/hgtid/TCGA-BA-5557
 #'@RG\tID:foo\tSM:bar'
 #SAMPLE=TCGA-BA-5557
